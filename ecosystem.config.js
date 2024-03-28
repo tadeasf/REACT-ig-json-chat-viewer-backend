873dc5c7223e9ed2b4af2a9031b38378b0fcb9e0
Tadeáš <taddy.fort@gmail.com>
2024-03-28 17:24:30 +0100

    Add pm2 to package.json and update app name in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   apps: [
     {
-      name: "fb-exp",
+      name: "messenger-backend",
       script: "./index.js", // Replace with the path to your server file
       instances: "1", // This will use all available CPU cores
       env: {
