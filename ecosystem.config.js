e4c32f7e6c0166cf875793fb9ae585f9e3d79539
Tadeáš <taddy.fort@gmail.com>
2024-03-29 21:05:05 +0100

    Add exec_mode: "fork" to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
       name: "a-messenger-backend",
       script: "./index.js",
       instances: "1",
+      exec_mode: "fork",
       env: {
         NODE_ENV: "production",
       },
